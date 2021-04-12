a083a3aa413037d86ba2d94877bc0002c3614e09
Temidayojnr <temidayojunior@gmail.com>
2021-04-12 20:41:59 +0100

    Added more dashboard cards.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,10 +1,11 @@
 <template>
-    <v-navigation-drawer v-model="drawer" permanent color="#F4F5F9" app>
+    <v-navigation-drawer permanent color="#F4F5F9" app>
+        <v-toolbar-side-icon class="black--text"></v-toolbar-side-icon>
         <v-list-item class="px-2 py-5">
             <v-list-item-title class="text-capitalize" align="center">
                 <v-img src="mswallet.png" height="80" width="200"></v-img>
                 <!-- <strong><h2 style="font-family: Fira sans">Master-Wallet</h2></strong> -->
-                <br><br>
+                
             </v-list-item-title>
         </v-list-item>
         <v-list nav dense>
@@ -19,32 +20,6 @@
                 </v-list-item>
             </v-list-item-group>
         </v-list>
-        <!-- <template v-slot:append>
-            <div class="pa-2">
-                <v-card align="center" class="pa-3">
-                    <v-badge bordered bottom color="green" dot offset-x="10" offset-y="10" class="mb-8">
-                        <v-avatar size="40">
-                            <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
-                        </v-avatar>
-                    </v-badge>
-                    <h4 class="grey--text">Total Transactions</h4>
-                    <h2>$500.50</h2>
-                    <h5 class="green-text">$400.50 today</h5>
-                    <v-card color="deep-purple" dark-class="mt-5">
-                        <v-list-item two-line>
-                            <v-list-item-content>
-                                <v-list-item-title>
-                                    Wallet Address
-                                </v-list-item-title>
-                                <v-list-item-subtitle>
-                                    3FZbgi29cpjq2GjdwV8eyHuJJnk
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-card>
-                </v-card>
-            </div>
-        </template> -->
     </v-navigation-drawer>
 </template>
 
@@ -52,14 +27,20 @@
 export default {
     data: () => ({
         selectedItem: 0,
-        drawer: null,
+        drawer: false,
+        group: null,
         items: [
             {icon: 'fas fa-home', text: 'Dashboard', route: '/'},
             {icon: 'fas fa-users', text: 'Users', route: '/users'},
             {icon: 'fas fa-dolly', text: 'Transactions', route: '/transactions'},
         ]
 
-    })
+    }),
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -154,7 +154,120 @@
           </v-card>
         </v-col>
 
-        <v-col></v-col>
+        <v-col cols="12" sm="6">
+          <v-card>
+            <v-app-bar flat color="rgba(0,0,0,0)">
+              <v-toolbar-title class="title black--text pl-0 ml-2">
+                Activity
+              </v-toolbar-title>
+            </v-app-bar>
+            <v-app-bar flat color="rgba(0,0,0,0)">
+              <v-btn fab color="purple lighten-4" elevation="0">
+                <v-icon color="purple">fab fa-bitcoin</v-icon>
+              </v-btn>
+              <v-toolbar-title class="title black--text ml-2">
+                Sent Bitcoin <span class="caption"><br>To Bitcoin Address</span>
+              </v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-title class="subtitle-1 black--text ml-2">
+                -0.00257 BTC
+              </v-toolbar-title>
+              <v-icon color="green" class="ml-2">fas fa-long-arrow-alt-up</v-icon>
+            </v-app-bar>
+             <v-app-bar flat color="rgba(0,0,0,0)">
+              <v-btn fab color="green lighten-4" elevation="0">
+                <v-icon color="green">fas fa-yen-sign</v-icon>
+              </v-btn>
+              <v-toolbar-title class="title black--text ml-2">
+                Receive Lightcoin <span class="caption"><br>Pending</span>
+              </v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-title class="subtitle-1 black--text ml-2">
+                -0.0133643 LTC
+              </v-toolbar-title>
+              <v-icon color="red" class="ml-2">fas fa-long-arrow-alt-down</v-icon>
+            </v-app-bar>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-card height="180px">
+            <v-row>
+              <v-col cols="12" sm="4">
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <div class="mb-4">Bitcoin</div>
+                    <v-list-item-title class="headline mb-1 black--text">
+                      2.5578 BTC
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="grey--text">$2500 USD</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+              <v-col cols="12" sm="8">
+                <v-sparkline
+                :value="value"
+                :smooth="radius || false"
+                :padding="padding"
+                :line-width="width"
+                :stroke-linecap="lineCap"
+                :fill="fills"
+                :type="type"
+                :auto-line-width="autoLineWidth"
+                auto-draw
+                color="red"
+                >
+
+                </v-sparkline>
+                <v-card-actions class="justify-end">
+                  <v-btn text color="green">
+                    <v-icon class="mt-n2 pr-2">fas fa-sort-down</v-icon> 8.5
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-card>
+            <v-row>
+              <v-col cols="12" sm="6" class="border">
+                <v-list-item three-line>
+                  <v-list-item-content>
+                    <div class="mb-4">Your Portfolio</div>
+                    <v-row>
+                      <v-col cols="12" sm="6">
+                        <v-progress-circular
+                        rotate="360"
+                        size="100"
+                        width="15"
+                        value="70"
+                        color="teal"
+                        >
+                        70%
+                        </v-progress-circular>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-list-item-subtitle class="grey--text mt-8">2.5578 BTC</v-list-item-subtitle>
+                        <v-list-item-subtitle class="grey--text">2500 USD</v-list-item-subtitle>
+                      </v-col>
+                    </v-row>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+              <v-col cols="12" sm="6" align="center">
+                <v-list-item two-line class="mt-10">
+                  <v-list-item-content>
+                     <v-list-item-title class="headline mb-1 black--text">
+                    $8520.50
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="grey--text">Pending Balance</v-list-item-subtitle>
+                  </v-list-item-content>
+                 
+                </v-list-item>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -176,8 +289,10 @@ import SideBar from '../components/SideBar'
       value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
       width: 2,
       lineCap: 'round',
+      type: 'trend',
       autoLineWidth: false,
-      fills: false
+      fills: false,
+      drawer: false
     })
   }
 </script>
